abbc89717ac79ce46ad78028e3ebcac43d529be2
SpreedBlood <spreed.blood@hotmail.com>
2017-05-29 23:52:21 +0200

    Search users and list friends

---
+++ Network/Game/Packets/Events/Messenger/HabboSearchMessageEvent.cs
@@ -0,0 +1,15 @@
+﻿using AuroraEmu.Game.Clients;
+using AuroraEmu.Game.Messenger;
+
+namespace AuroraEmu.Network.Game.Packets.Events.Messenger
+{
+    public class HabboSearchMessageEvent : IPacketEvent
+    {
+        public void Run(Client client, MessageEvent msg)
+        {
+            string search = msg.ReadString();
+            MessageComposer message = MessengerController.GetInstance().MessengerSearch(search, client);
+            client.SendComposer(message);
+        }
+    }
+}
