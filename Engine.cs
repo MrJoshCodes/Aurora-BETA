c60d41f33693ad2f8140d2a24d17921388ddde13
MrJoshCodes <d197386@edu.rocwb.nl>
2017-10-20 21:44:39 +0200

    Added fix to save clothes on d/c.

--- Engine.cs
+++ Engine.cs
@@ -148,6 +148,7 @@ namespace AuroraEmu
         {
             Locator.ClientController.Dispose();
             Locator.GameNetworkListener.Dispose();
+            Environment.exit();
         }
     }
 }
